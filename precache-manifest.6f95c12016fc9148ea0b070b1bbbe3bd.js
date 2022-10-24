08578b95813238312bed4b0cab4ea71f20817f33
jxzper <jesper.hoogenberg@gmail.com>
2022-10-24 02:59:02 +0200

    Updates

---
+++ precache-manifest.6f95c12016fc9148ea0b070b1bbbe3bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e43d521ef9f47546891f6e18ff271a8",
+    "url": "/embedbuilder/index.html"
+  },
+  {
+    "revision": "9d674352b5453fa6a1d6",
+    "url": "/embedbuilder/static/css/main.545bca7a.chunk.css"
+  },
+  {
+    "revision": "93bd13c7ea9047b17a3e",
+    "url": "/embedbuilder/static/js/2.3c2068e2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/embedbuilder/static/js/2.3c2068e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d674352b5453fa6a1d6",
+    "url": "/embedbuilder/static/js/main.d38647be.chunk.js"
+  },
+  {
+    "revision": "c31fa48d8b0f4950ba45",
+    "url": "/embedbuilder/static/js/runtime-main.682d2db8.js"
+  }
+]);
