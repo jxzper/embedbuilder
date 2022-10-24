107b8fbc57d8bcc0c92624c73d6d90f664b52087
jxzper <jesper.hoogenberg@gmail.com>
2022-10-24 03:13:07 +0200

    small fixes

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 class Footer extends React.Component {
   render() {
     return (
-      <footer>
-        <div className="credits">Credits to: The Bastion Bot Project</div>
+      <footer>     
+        <a href="https://github.com/TheBastionBot/embedbuilder">Credits to: The Bastion Bot</a>
       </footer>
     );
   }
